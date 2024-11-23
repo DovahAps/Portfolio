34882f48eb1f7a4acf19038d02bf5a81cb08638f
Marcelo Alves <85163936+DovahAps@users.noreply.github.com>
2024-11-23 16:07:27 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.module.scss';
 import BaseLayout from "./components/BaseLayout";
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
    return (
       <div>
-         <BrowserRouter>
-            <BaseLayout/>
+         <BrowserRouter basename="/Portfolio">
+            <BaseLayout />
          </BrowserRouter>
       </div>
    );
 }
 
-
 export default App;
+
